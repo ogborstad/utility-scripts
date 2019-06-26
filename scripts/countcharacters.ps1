2467546e18418cd72e1c6ad4ce6ee2c403697602
Ole Gunnar Borstad <ole.borstad@evry.com>
2019-06-26 10:02:52 +0200

    Avoid using powershell alias select

--- scripts/countcharacters.ps1
+++ scripts/countcharacters.ps1
@@ -1,3 +1,3 @@
 $string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-$characterCount = $string | measure-object -character | select -expandproperty characters
+$characterCount = $string | measure-object -character | Select-Object -expandproperty characters
 Write-Host $characterCount;
